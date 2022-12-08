d2d1df1a5d5bf76870347a840417d263c5b3f7b0
leezer94 <2kunhee94@gmail.com>
2022-12-08 17:39:53 +0900

    Feat : 버튼 컴포넌트 구성

--- src/components/@commons/Button/Button.style.ts
+++ src/components/@commons/Button/Button.style.ts
@@ -1,25 +1,22 @@
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 
 const ShadowButton = styled.button`
-  ${({ theme }) => css`
-    width: 300px;
-    font-size: 18px;
-    cursor: pointer;
-    padding: '20px 40px';
-    border: 1px solid none;
-    border-radius: 8px;
-    background-color: ${theme.BLUE}
-    box-shadow: '20px 30px 30px 0 rgb(0 31 68 / 30%)';
-    color: white;
-    font-weight: 500;
-    text-align: center;
-    display: inline-block;
-    text-decoration: none;
+  width: 200px;
+  height: 50px;
+  font-size: 18px;
+  cursor: pointer;
+  padding: 10px 40px;
+  border: 1px solid none;
+  border-radius: 8px;
+  background-color: ${({ theme }) => theme.BLUE};
+  box-shadow: 20px 30px 30px 0 rgb(0 31 68 / 30%);
+  color: white;
+  font-weight: 500;
+  text-align: center;
+  display: inline-block;
+  text-decoration: none;
 
-    width: 90%;
-
-    letter-spacing: 1px;
-  `}
+  letter-spacing: 1px;
 `;
 
 export { ShadowButton };
--- src/components/@commons/Button/ShadowButton.tsx
+++ src/components/@commons/Button/ShadowButton.tsx
@@ -2,10 +2,15 @@ import * as S from './Button.style';
 
 type ShadowButton = {
   content: string;
+  onClick?: () => void;
 };
 
-const ShadowButton = ({ content }: ShadowButton) => {
-  return <S.ShadowButton type='button'>{content}</S.ShadowButton>;
+const ShadowButton = ({ content, ...rest }: ShadowButton) => {
+  return (
+    <S.ShadowButton type='button' {...rest}>
+      {content}
+    </S.ShadowButton>
+  );
 };
 
 export default ShadowButton;

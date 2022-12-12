6b0024f74c6f993b80040b45b65d32061d415294
leezer94 <2kunhee94@gmail.com>
2022-12-12 17:44:46 +0900

    Feat : 뒤로가기 버튼 추가

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,13 +1,24 @@
 import ShadowCard from '@/components/@commons/Card/ShadowCard';
 import PageWrapper from '@/components/PageWrapper/PageWrapper';
 import SignInForm from '@/components/Auth/SignIn/SignIn';
+import ShadowButton from '@/components/@commons/Button/ShadowButton';
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
+  const navigate = useNavigate();
+
   return (
     <PageWrapper>
-      <ShadowCard width='100%' height='500px' title='로그인 페이지'>
-        <SignInForm />
-      </ShadowCard>
+      <>
+        <ShadowButton
+          type='button'
+          content='뒤로가기'
+          onClick={() => navigate(-1)}
+        />
+        <ShadowCard width='100%' height='500px' title='로그인 페이지'>
+          <SignInForm />
+        </ShadowCard>
+      </>
     </PageWrapper>
   );
 };
--- src/pages/SignUp/SIgnUp.tsx
+++ src/pages/SignUp/SIgnUp.tsx
@@ -1,13 +1,24 @@
 import PageWrapper from '@/components/PageWrapper/PageWrapper';
 import ShadowCard from '@/components/@commons/Card/ShadowCard';
 import SignUpForm from '@/components/Auth/SignUp/SignUp';
+import ShadowButton from '@/components/@commons/Button/ShadowButton';
+import { useNavigate } from 'react-router-dom';
 
 const SignUp = () => {
+  const navigate = useNavigate();
+
   return (
     <PageWrapper>
-      <ShadowCard width='100%' height='500px' title='회원가입 페이지'>
-        <SignUpForm />
-      </ShadowCard>
+      <>
+        <ShadowButton
+          type='button'
+          content='뒤로가기'
+          onClick={() => navigate(-1)}
+        />
+        <ShadowCard width='100%' height='500px' title='회원가입 페이지'>
+          <SignUpForm />
+        </ShadowCard>
+      </>
     </PageWrapper>
   );
 };
